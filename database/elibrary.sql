6bd55020677f16ca49896b83d74d1de5c0d9291c
piya-prue <34237478+piya-prue@users.noreply.github.com>
2019-03-25 00:14:46 +0600

    database for sprint 1

--- database/elibrary.sql
+++ database/elibrary.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2019 at 01:51 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- Generation Time: Mar 23, 2019 at 09:07 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,24 +29,27 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `person` (
-  `person_id` int(10) NOT NULL,
+  `person_id` int(5) NOT NULL,
   `person_name` varchar(100) NOT NULL,
-  `person_academics` varchar(100) NOT NULL,
   `person_email` varchar(100) NOT NULL,
-  `person_phone` varchar(20) NOT NULL,
-  `person_type` varchar(50) NOT NULL,
-  `person_password` varchar(50) NOT NULL
+  `person_password` varchar(100) NOT NULL,
+  `person_phone` varchar(100) NOT NULL,
+  `person_address` varchar(100) NOT NULL,
+  `person_profession` varchar(100) NOT NULL,
+  `person_experience` varchar(100) NOT NULL,
+  `person_university` varchar(100) NOT NULL,
+  `person_department` varchar(100) NOT NULL,
+  `person_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `person`
 --
 
-INSERT INTO `person` (`person_id`, `person_name`, `person_academics`, `person_email`, `person_phone`, `person_type`, `person_password`) VALUES
-(1, 'Piya Prue Marma', 'Sayednagar', 'piya@gmail.com', '123456789', 'student', '1234'),
-(2, 'Omar Faruk', 'United International University', 'omar@gmail.com', '987654321', 'student', '4321'),
-(4, 'Aditi Debnath', '', 'aditi@gmail.com', '', 'Student', '4567'),
-(6, 'Zahid Hossain', '', 'zahid@gmail.com', '', 'Student', '0000');
+INSERT INTO `person` (`person_id`, `person_name`, `person_email`, `person_password`, `person_phone`, `person_address`, `person_profession`, `person_experience`, `person_university`, `person_department`, `person_type`) VALUES
+(1, 'Piya Prue Marma', 'piya@gmail.com', '1234', '01670898699', 'Sayed Nagar, Dhaka', 'Student', '2 years', 'United International University', 'CSE', 'Student'),
+(2, 'Swakkhar Shatabda', 'swakkhar@gmail.com', '0000', '01836105233', 'Madani Avenue, Dhaka', 'Lecturer', '5 years', 'United International Unviersity', 'CSE', 'Teacher'),
+(4, 'Aditi Debnath', 'aditi@gmail.com', '1111', '6547893211', 'sayed nagar', '', '', 'UIU', 'CSE', 'Guest');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +69,7 @@ ALTER TABLE `person`
 -- AUTO_INCREMENT for table `person`
 --
 ALTER TABLE `person`
-  MODIFY `person_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `person_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
