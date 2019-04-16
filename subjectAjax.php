9b7fe2ba57bcac8c8fade2faa8468824a23a3a43
OmarFaruque96 <40633797+OmarFaruque96@users.noreply.github.com>
2019-04-16 22:54:24 +0600

    ajax for topic selection

--- subjectAjax.php
+++ subjectAjax.php
@@ -1,14 +1,15 @@
 <?php
 	include('connection.php');
 
-	$i=1;
-	$q=intval($_GET['data']);
-	echo $q;
-	$query="SELECT * FROM subject WHERE class_id='$q'";
+	if(isset($_GET['data'])){
+      $q=$_GET['data'];
+    }
+	
+	$query="SELECT subject_name FROM subject WHERE class_id=(SELECT class_id FROM class WHERE class_name='$q')";
         $result=mysqli_query($conn,$query);
         if(mysqli_num_rows($result) > 0){
 			while($row = mysqli_fetch_array($result))
-			echo"<option value=". $i++ .">" .$row['subject_name']."</option>";
+			echo"<option value=".$row['subject_name'].">" .$row['subject_name']."</option>";
 		}	
 	mysqli_close($con);
 ?>
