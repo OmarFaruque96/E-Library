5fac65ea55ab6eb501fba30b1ab835f3687ebf2f
MahediO98 <47682532+MahediO98@users.noreply.github.com>
2019-04-17 12:56:37 +0600

    Add files via upload

---
+++ regular-pdf-page.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+   
+    <div id="preloader">
+        <div class="spinner"></div>
+    </div>
+
+    <header class="header-area">
+
+        <!-- Top Header Area -->
+        <div class="top-header-area d-flex justify-content-between align-items-center">
+            <!-- Contact Info -->
+            <div class="contact-info">
+                <a href="#"><span>Phone:</span> +880 1773 357272</a>
+                <a href="#"><span>Email:</span> engineerbrother.cse@gmail.com</a>
+            </div>
+            <!-- Follow Us -->
+            <div class="follow-us">
+                <span>Follow us</span>
+                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+        </div>
+
+        <!-- Navbar Area -->
+        <div class="clever-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <!-- Menu -->
+                <nav class="classy-navbar justify-content-between" id="cleverNav">
+
+                    <!-- Logo -->
+                    <a class="nav-brand" href="index.html"><img src="img/core-img/logo.png" alt=""></a> 
+
+                    <!-- Navbar Toggler -->
+                    <div class="classy-navbar-toggler">
+                        <span class="navbarToggler"><span></span><span></span><span></span></span>
+                    </div>
+
+                    <!-- Menu -->
+                    <div class="classy-menu">
+
+                        <!-- Close Button -->
+                        <div class="classycloseIcon">
+                            <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                        </div>
+
+                        <!-- Nav Start -->
+                        <div class="classynav">
+                            <ul>
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="#">Books</a>
+                                    <ul class="dropdown">
+										<li><a href="#">English Medium</a>
+											<ul class="dropdown">
+													<li><a href="">Class 1-7</a>
+														<ul class="dropdown">
+																<li><a href="">Class 1</a></li>
+																<li><a href="">Class 2</a></li>
+																<li><a href="">Class 3</a></li>
+																<li><a href="">Class 4</a></li>
+																<li><a href="">Class 5</a></li>
+																<li><a href="">Class 6</a></li>
+																<li><a href="">Class 7</a></li>
+														</ul>
+													</li>
+													<li><a href="">O-Level</a>
+														<ul class="dropdown">
+															<li><a href="">Class 8</a></li>
+															<li><a href="">Class 9</a></li>
+															<li><a href="">Class 10</a></li>
+														</ul>
+													</li>
+													<li><a href="">A-Level</a>
+														<ul class="dropdown">
+															<li><a href="">Class 11</a></li>
+															<li><a href="">Class 12</a></li>
+														</ul>
+													</li>
+											</ul>
+										</li>
+										<li><a href="#">Bangla Medium</a>
+											<ul class="dropdown">
+													<li><a href="">Primary</a>
+														<ul class="dropdown">
+																<li><a href="">Class 1</a></li>
+																<li><a href="">Class 2</a></li>
+																<li><a href="">Class 3</a></li>
+																<li><a href="">Class 4</a></li>
+																<li><a href="">Class 5</a></li>
+														</ul>
+													</li>
+													<li><a href="">Secondary</a>
+														<ul class="dropdown">
+																<li><a href="">Class 6</a></li>
+																<li><a href="">Class 7</a></li>
+																<li><a href="">Class 8</a></li>
+																<li><a href="">Class 9</a></li>
+																<li><a href="">Class 10</a></li>
+														</ul>
+													</li>
+													<li><a href="">Higher Secondary</a>
+														<ul class="dropdown">
+																<li><a href="">Class 11</a></li>
+																<li><a href="">Class 12</a></li>
+														</ul>
+													</li>
+											</ul>
+										</li>
+                                        <li><a href="single-course.html">Undergraduate</a>
+											<ul class="dropdown">
+													<li><a href="">Engineering</a>
+														<ul class="dropdown">
+																<li><a href="">CSE</a></li>
+																<li><a href="">EEE</a></li>
+																<li><a href="">CTE</a></li>
+																<li><a href="">IPE</a></li>
+														</ul>
+													</li>
+													<li><a href="">BBA</a>
+														<ul class="dropdown">
+																<li><a href="">Subject - 01</a></li>															
+														</ul>
+													</li>
+													<li><a href="">Economics</a>
+														<ul class="dropdown">
+																<li><a href="">Economics</a></li>
+														</ul>
+													</li>
+													<li><a href="">Business Studies</a>
+														<ul class="dropdown">
+																<li><a href="">Business</a></li>
+														</ul>
+													</li>
+											</ul>
+										</li>
+										<li><a href="#">Others</a>
+											<ul class="dropdown">
+													<li><a href="">IELTS</a></li>
+													<li><a href="">GRE</a></li>
+													<li><a href="">TOFEL</a></li>
+													<li><a href="">Story</a></li>
+											</ul>
+										</li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Notes & Slides</a>
+                                    <ul class="dropdown">
+										<li><a href="#">English Medium</a>
+											<ul class="dropdown">
+													<li><a href="">O-Level</a></li>
+													<li><a href="">A-Level</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Bangla Medium</a>
+											<ul class="dropdown">
+													<li><a href="">Primary</a></li>
+													<li><a href="">Secondary</a></li>
+													<li><a href="">Higher Secondary</a></li>
+											</ul>
+										</li>
+                                        <li><a href="single-course.html">Undergraduate</a>
+											<ul class="dropdown">
+													<li><a href="">CSE</a></li>
+													<li><a href="">EEE</a></li>
+													<li><a href="">ECE</a></li>
+													<li><a href="">BBA</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Others</a>
+											<ul class="dropdown">
+													<li><a href="">IELTS</a></li>
+													<li><a href="">GRE</a></li>
+													<li><a href="">TOFEL</a></li>
+													<li><a href="">Story</a></li>
+											</ul>
+										</li>
+                                    </ul>
+                                </li>
+                                
+                                <li><a href="">Question Bank</a></li>
+                                <li><a href="">Blog</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+
+                            <!-- Search Button -->
+                            <div class="search-area">
+                                <form action="#" method="post">
+                                    <input type="search" name="search" id="search" placeholder="Search">
+                                    <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                </form>
+                            </div>
+
+                            <!-- Register / Login -->
+                            <div class="register-login-area">
+                                <a href="signup.html" class="btn">Register</a>
+                                <a href="login.html" class="btn active">Login</a>
+                            </div>
+
+                        </div>
+                        <!-- Nav End -->
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+   
+    <div class="regular-page-area section-padding-100">
+        <div class="container">
+		
+		<?php 
+						$sql = "SELECT link FROM gallery WHERE type_id=2 AND pdf_title='Demo'";
+                                    $result = $conn->query($sql);
+                                    $videos = [];
+                                    $count = 0;
+                                    
+                                    if($result->num_rows > 0){
+                                        $row = $result->fetch_assoc();
+                                            
+                                                $title = $row["pdf_title"];
+                                                //$writer = $row["writer_name"];
+												$link = $row["link"];
+                                            
+                                
+                                    }
+                                    else{
+                                        echo "0 results";
+                                    }
+					?>
+		
+            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+			
+					<iframe class="embed-responsive-item" src="<?php echo "pdf notes/".$link ?>" frameborder="0"  width="100%" allowfullscreen> </iframe>
+					
+			</div>
+        </div>
+    </div>
+  
+  
+  
+    <footer class="footer-area">
+        <!-- Top Footer Area -->
+        <div class="top-footer-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <!-- Footer Logo -->
+                        <div class="footer-logo">
+                            <!-- <a href="index.html"><img src="img/core-img/logo2.png" alt=""></a> -->
+                        </div>                     
+                        <p><a href="#">
+						Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <i class="" aria-hidden="true"></i> by <a href="" target="_blank">Engineer Brother Company</a>
+						</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bottom Footer Area -->
+        <div class="bottom-footer-area d-flex justify-content-between align-items-center">
+            <!-- Contact Info -->
+            <div class="contact-info">
+                <a href="#"><span>Phone:</span> +880 1773 357272</a>
+                <a href="#"><span>Email:</span> engineerbrother.cse@gmail.com</a>
+            </div>
+            <!-- Follow Us -->
+            <div class="follow-us">
+                <span>Follow us</span>
+                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/plugins/plugins.js"></script>
+    <script src="js/active.js"></script>
+</body>
+
+</html>
