119a1c97d6440ede35af855573b2aae747117280
sadid <sadid.hasan14@gmail.com>
2019-04-30 00:00:26 +0600

    taking user address from the database but can not draw marker

---
+++ map.php
@@ -0,0 +1,95 @@
+<?php
+include ('connection.php');
+
+session_start();
+$session_data = $_SESSION['email'];
+
+$sql = "SELECT * FROM person WHERE person_email='$session_data'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $address = $row['person_address'];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <title>EduSolution | Location</title>
+</head>
+
+<body>
+    <div id="maparea" style="height: 715px"></div>
+    <script>
+        var map;
+        function drawmap() {
+            map = new google.maps.Map(document.getElementById('maparea'), {
+                center: { lat: 23.42, lng: 90.50 },
+                zoom: 10,
+                mapTypeId: 'hybrid'
+            });
+
+            map.addListener("click", drawMarker);
+        }
+
+        geocode();
+
+        function geocode() {
+            var location = "<?php echo $address ?>";
+            console.log(location);
+            axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address: location,
+                    key: 'AIzaSyC4R_TR6n5C2k6qYsA_fIH-rvZX5kPTM9Q'
+                }
+            })
+                .then(function (response) {
+                    // Formatted Address
+                    var formattedAddress = response.data.results[0].formatted_address;
+                    console.log(formattedAddress);
+
+                    // Geometry
+                    var latitude = response.data.results[0].geometry.location.lat;
+                    var longitude = response.data.results[0].geometry.location.lng;
+
+                    console.log(latitude);
+                    console.log(longitude);
+
+                    var marker = new google.maps.Marker({
+                        position: { lat: latitude, lng: longitude },
+                        map: map
+                    });
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+
+        function drawMarker(event) {
+            var pos = event.latLng;
+
+            var newmk = new google.maps.Marker({
+                position: pos,
+                map: map,
+                draggable: true
+            });
+
+            newmk.addListener("dragend", dragEvent);
+        }
+
+        function dragEvent(event) {
+            window.alert(event.latLng.lat() + " " + event.latLng.lng());
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R_TR6n5C2k6qYsA_fIH-rvZX5kPTM9Q&callback=drawmap"></script>
+</body>
+
+</html>
--- profile.php
+++ profile.php
@@ -43,7 +43,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>EduSolution | Profile</title>
     <link rel="icon" href="img/core-img/favicon.ico">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -292,7 +293,10 @@
 								</a>
 								<a href="">
 									<i style="font-size:28px ;padding:10px 10px" class="fa">&#xf0e0;</i>
-								</a> 
+                                </a> 
+                                <a href="map.php">
+                                    <i class="fas fa-map-marker-alt" style="font-size:28px;padding:10px 10px"></i>
+                                </a>
 							</div>
 							
 							<div>
