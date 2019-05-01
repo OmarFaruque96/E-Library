c08689f81158dc00c3471a0c4c31cb88349ba367
OmarFaruque96 <40633797+OmarFaruque96@users.noreply.github.com>
2019-05-02 00:42:54 +0600

    sign up page with validation

--- signup.php
+++ signup.php
@@ -1,24 +1,87 @@
 <?php 
-  session_start();
+ session_start();
   include("connection.php");
-
-  if(isset($_POST['register_button'])){
-    $name=$_POST['name']; 
-    $email=$_POST['email']; 
-    $phone=$_POST['phone']; 
-    $type=$_POST['type']; 
-    $password=$_POST['password']; 
-
-    if($type == ""){
-      $type = "Guest";
-    }
-
-    $query="INSERT INTO person (person_name, person_email, person_phone, person_type, person_password) 
+  
+	$nameError ="";
+	$emailError ="";
+	$phoneError ="";
+	$passError ="";
+	$name="";
+	$email="";
+	$password="";
+	$type="";
+	$phone="";
+		
+		// On submitting form below function will execute.
+	if(isset($_POST['register_button'])){
+		if (empty($_POST["name"])) {
+			$nameError = "Name is required";
+		} else {
+			$name = test_input($_POST["name"]);
+			// check name only contains letters and whitespace
+			if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+			$nameError = "Only letters and white space allowed";
+			}
+		}
+		if (empty($_POST["email"])) {
+			$emailError = "Email is required";
+		} else {
+			$email = test_input($_POST["email"]);
+			// check if e-mail address syntax is valid or not
+			$demo="Invalid email format";
+			
+			$query="SELECT * FROM person WHERE person_email='$demo'";
+			
+			$result=mysqli_query($conn,$query);
+			if(mysqli_num_rows($result) > 0){
+				$row=mysqli_fetch_assoc($result);
+				if($row['sector_id']==$email){
+					$emailError="already have an account";
+				}
+				
+			}	
+				
+			if (!preg_match("/([w-]+@[w-]+.[w-]+)/",$email)) {
+				//$emailError = $demo;
+			}
+		}
+		if (empty($_POST["phone"])) {
+			$phoneError = "phone is required";
+		} else {
+			$phone = test_input($_POST["phone"]);
+			if(strlen($phone)!= 11){
+				$phoneError = "Invalid";
+			}
+			// check name only contains letters and whitespace
+			if (!preg_match("/^[0-9]*$/",$phone)) {
+				$phoneError = "Only numbers allowed";
+			}
+		}
+		if (empty($_POST["password"])) {
+			$passError = "phone is required";
+		} else {
+			$password = test_input($_POST["password"]);
+			if(strlen($password)<6){
+				$passError = "atleast 6 characters ";
+			}
+		}
+		//$phone=$_POST['phone']; 
+		$type=$_POST['type']; 
+		$password=$_POST['password']; 
+	}
+  
+  function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+	}
+  
+  
+  $query="INSERT INTO person (person_name, person_email, person_phone, person_type, person_password) 
 			VALUES ('$name', '$email', '$phone', '$type', '$password');";
     mysqli_query($conn,$query);
-    header("Location:login.php");
-  }
-  
+	
 ?>
 
 <!DOCTYPE html>
@@ -229,24 +292,29 @@
 					<p class="divider-text">
 						<span class="bg-light">OR</span>
 					</p>
-					<form action="" method="post">
+					<form action="dc.php" method="post">
+					
+					
 					<div class="form-group input-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"> <i class="fa fa-user"></i> </span>
 						 </div>
-						<input name="name" class="form-control" placeholder="Full name" type="text" required>
+						<input name="name" class="form-control" placeholder="Full name <?php echo $nameError;?>" type="text" required>
+						
 					</div> <!-- form-group// -->
 					<div class="form-group input-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
 						 </div>
-						<input name="email" class="form-control" placeholder="Email address" type="email" required>
+						<input name="email" class="form-control" placeholder="Email address"<?php echo $emailError;?> type="email" required>
+						
 					</div> <!-- form-group// -->
 					<div class="form-group input-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"> <i class="fa fa-phone"></i> </span>
 						</div>
-						<input name="phone" class="form-control" placeholder="Phone number" type="text" required>
+						<input name="phone" class="form-control" placeholder="Phone number"<?php echo $phoneError;?> type="text" required>
+						
 					</div> <!-- form-group// -->
 					<div class="form-group input-group">
 						<div class="input-group-prepend">
@@ -263,7 +331,8 @@
 						<div class="input-group-prepend">
 							<span class="input-group-text"> <i class="fa fa-lock"></i> </span>
 						</div>
-						<input name="password" class="form-control" placeholder="Create password" type="password" required>
+						<input name="password" class="form-control" placeholder="Create password"<?php echo $passError;?> type="password" required>
+						
 					</div> <!-- form-group// -->                                      
 					<div class="form-group">
 					
